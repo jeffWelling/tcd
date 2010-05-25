54384f6d7594676b8ebc716d98fbda9fa66b8dc5
Jeff Welling <Jeff.Welling@Gmail.com>
2010-05-24 19:12:22 -0700

    Revert "Revert "Re-adding superfluous variable, pulling loop onto one line""
    This reverts commit 77156b2210b95759184f49c66a934f2630a51d60.

--- lib/tcd/IRB.rb
+++ lib/tcd/IRB.rb
@@ -23,9 +23,8 @@ module TCD
       include TCD::Profiles
       def getAllProfileStats
         profiles = Profiles.profiles.select {|profile| profile.useProfile? }
-        TCD::Storage.saveStats profiles.collect {|profile|
-          ["#{profile}"[/[^:]+?$/].to_sym], profile.getStats.merge({:timestamp=>Time.now})]
-        }.to_h
+        stats = profiles.collect {|profile| ["#{profile}"[/[^:]+?$/].to_sym], profile.getStats.merge({:timestamp=>Time.now})] }.to_h
+        TCD::Storage.saveStats stats
       end
       #Return the total number of bytes used this billing cycle
       def usageThisBillingPer profile_name, interface
